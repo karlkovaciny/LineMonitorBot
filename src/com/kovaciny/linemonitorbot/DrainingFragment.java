bb8aca293d53a5f2e3e3fc231aec0525d5846412
Karl <fireslinger@gmail.com>
2013-05-02 09:24:33 -0500

    A couple test layouts.

--- src/com/kovaciny/linemonitorbot/DrainingFragment.java
+++ src/com/kovaciny/linemonitorbot/DrainingFragment.java
@@ -1,9 +1,13 @@
 package com.kovaciny.linemonitorbot;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class DrainingFragment extends SectionFragment {
 	
@@ -14,6 +18,40 @@ public class DrainingFragment extends SectionFragment {
 		View rootView = inflater.inflate(R.layout.draining_fragment,
 				container, false);
 		
+		final ListView testList = (ListView) rootView.findViewById(R.id.listView1);
+		final Button myButton = (Button)rootView.findViewById(R.id.button1);
+		myButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+			    myButton.setText("You click...");
+			    // SLEEP 2 SECONDS HERE ...
+			    Handler handler = new Handler(); 
+			    handler.postDelayed(new Runnable() { 
+			         public void run() { 
+			              myButton.setText("and I respond."); 
+			         } 
+			    }, 2000); 
+				
+			}
+		});
+		
+		String testStrings[] = {"one","two","three", "four","five","six","seven","eight","nine","ten"};
+		
+		
+		ArrayAdapter<String> testAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, testStrings);
+		testList.setAdapter(testAdapter);
+		
+	    // SLEEP 2 SECONDS HERE ...
+	    Handler handler = new Handler(); 
+	    handler.postDelayed(new Runnable() { 
+	         public void run() { 
+	        		Integer testArray[] = {1,2,3,4,5,6,7,8,9,10,11,12};
+	        		ArrayAdapter<Integer> secondAdapter = new ArrayAdapter<Integer>(getActivity(), android.R.layout.simple_list_item_1, testArray);
+	        		testList.setAdapter(secondAdapter);
+	         } 
+	    }, 2000); 
+	    
 		return rootView;
 	}
 }
