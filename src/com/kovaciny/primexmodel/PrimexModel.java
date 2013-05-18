b90e64bea788409051d8dbc633cbeb3ab4634b80
Karl <fireslinger@gmail.com>
2013-05-18 04:25:28 -0500

    Create Model class

--- src/com/kovaciny/primexmodel/PrimexModel.java
+++ src/com/kovaciny/primexmodel/PrimexModel.java
@@ -1,5 +1,29 @@
 package com.kovaciny.primexmodel;
 
-public class PrimexModel {
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
+public class PrimexModel {
+	// Create PropertyChangeSupport to manage listeners and fire events.
+	  private final PropertyChangeSupport propChangeSupport = new PropertyChangeSupport(this);
+	  private int foo;
+	  
+	  // Provide delegating methods to add / remove listeners to / from the support class.  
+	  public void addPropertyChangeListener(PropertyChangeListener l) {
+	    propChangeSupport.addPropertyChangeListener(l);
+	  }
+
+	  public void removePropertyChangeListener(PropertyChangeListener l) {
+	    propChangeSupport.removePropertyChangeListener(l);
+	  }
+
+	  // Simple example of how to fire an event when the value of 'foo' is changed.
+	  protected void setFoo(int foo) {
+	    if (this.foo != foo) {
+	      // Remember previous value, assign new value and then fire event.
+	      int oldFoo = this.foo;
+	      this.foo = foo;
+	      propChangeSupport.firePropertyChange("foo", oldFoo, this.foo);
+	    }
+	  }
 }
