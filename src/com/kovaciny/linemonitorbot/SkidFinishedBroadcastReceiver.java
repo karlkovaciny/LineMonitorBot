02308e6a56997de254aa2defcf4dfbe8fcae8f4f
Karl <fireslinger@gmail.com>
2013-06-18 12:19:12 -0500

    Add one last warning on the vibrator alarm

--- src/com/kovaciny/linemonitorbot/SkidFinishedBroadcastReceiver.java
+++ src/com/kovaciny/linemonitorbot/SkidFinishedBroadcastReceiver.java
@@ -49,7 +49,7 @@ public class SkidFinishedBroadcastReceiver extends BroadcastReceiver {
 		 
 		         Toast.makeText(context, msgStr, Toast.LENGTH_LONG).show();
 //		         long[] offOnPattern = {0, 5000, 500, 3000, 300, 300, 300, 300, 300, 300, 300, 3000};
-		         long[] offOnPattern = {0, 300, 300, 300, 300, 2500, 300, 300, 300, 2500, 300, 300, 300, 2500, 300, 300, 300, 2500, 300, 300, 300, 2500};
+		         long[] offOnPattern = {0, 300, 300, 300, 300, 2500, 300, 300, 300, 2500, 300, 300, 300, 2500, 300, 300, 300, 2500, 300, 300, 300, 2500,30000,2000};
 		         vibe.vibrate(offOnPattern, -1);
 		         
 		     } finally {
