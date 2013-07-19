9ec952429f7b1a09108147890a5883e7f2626029
Karl <fireslinger@gmail.com>
2013-07-18 23:12:45 -0500

    Add unit tests

---
+++ src/tests/PrimexModelTest.java
@@ -0,0 +1,199 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PrimexModelTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testPrimexModel() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddPropertyChangeListener() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRemovePropertyChangeListener() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetSelectedLineInteger() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetSelectedLineProductionLine() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetSelectedWorkOrder() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddSkid() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddProduct() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddWorkOrder() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddWorkOrderWorkOrder() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetCurrentSpeed() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testChangeNovatecSetpoint() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testChangeProduct() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testChangeSelectedSkid() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSaveProduct() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSaveSelectedLine() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSaveSkid() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSaveState() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testLoadState() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetProductsPerMinute() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetProductsPerMinute() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCalculateRates() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCalculateTimes() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetDatabaseVersion() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testChangeNumberOfSkids() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCloseDb() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testHasSelectedLine() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testHasSelectedWorkOrder() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testHasSelectedProduct() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetSelectedLine() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetSelectedWorkOrder() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetLineNumbers() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetWoNumbers() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetAllWoNumbersForLine() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetHighestWoNumber() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testClearWoNumbers() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
