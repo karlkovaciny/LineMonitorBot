efe8532b8c68c525cc671d3a4383f114163e4fdc
Karl <fireslinger@gmail.com>
2013-09-26 10:22:40 -0500

    Start draining UI

--- src/com/kovaciny/linemonitorbot/DrainingFragment.java
+++ src/com/kovaciny/linemonitorbot/DrainingFragment.java
@@ -1,5 +1,8 @@
 package com.kovaciny.linemonitorbot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,16 +10,23 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class DrainingFragment extends Fragment {
+public class DrainingFragment extends Fragment implements View.OnClickListener {
 
     private Button mBtn_selfDestruct;
+    private Button mBtn_getDrainTimes;
+    
+    private List<EditText> mEditableGroup = new ArrayList<EditText>();
+    private EditText mEdit_grossRate;
+
+    private DrainingController mDrainingController;
     
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		
 		View rootView = inflater.inflate(R.layout.draining_fragment, container, false);
+		
 		mBtn_selfDestruct = (Button) rootView.findViewById(R.id.btn_self_destruct);
 		mBtn_selfDestruct.setOnClickListener(new OnClickListener() {
             
@@ -26,6 +36,39 @@ public class DrainingFragment extends Fragment {
             }
         });
 		
+		mBtn_getDrainTimes = (Button) rootView.findViewById(R.id.btn_get_drain_times);
+		mBtn_getDrainTimes.setOnClickListener(this);
+		
+		mEdit_grossRate = (EditText) rootView.findViewById(R.id.edit_gross_rate);
+		mEditableGroup.add(mEdit_grossRate);
+		
 		return rootView;
 	}
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_get_drain_times:
+                for (EditText et : mEditableGroup) {
+                    et.clearFocus();
+                    et.setError(null);
+                }
+                if (this.validateInputs()) {
+                    
+                }
+                break;
+        }
+    }
+    
+    private boolean validateInputs() {
+        boolean validInputs = true;
+        if (mEdit_grossRate.getText().length() == 0) {
+            mEdit_grossRate.setError(getString(R.string.error_empty_field));
+            validInputs = false;
+        } else if (mEdit_grossRate.getText().toString().equals("0")) {
+            mEdit_grossRate.setError(getString(R.string.error_need_nonzero));
+            validInputs = false;
+        }
+        return validInputs;
+    }
 }
