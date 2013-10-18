bd9d15e1ab81ef9373dc9de95d2651e895f34569
Karl <fireslinger@gmail.com>
2013-10-18 08:56:01 -0500

    Committing for computer conflicts.

---
+++ src/com/kovaciny/helperfunctions/SimplePaceDateFormat.java
@@ -0,0 +1,13 @@
+package com.kovaciny.helperfunctions;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+public class SimplePaceDateFormat extends SimpleDateFormat {
+	
+	public SimplePaceDateFormat(String pattern, Locale locale) {
+	}	
+}
